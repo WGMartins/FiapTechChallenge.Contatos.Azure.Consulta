00da53ac27923cf6ffeefa2206b6f976679f25df
Eduardo Queiroz <esqueiroz@LIVE.COM>
2025-03-21 23:08:10 -0300

    Azure Function para consulta de contatos por DDD

---
+++ FiapTechChallenge.Contatos.Azure.Consulta/ConsultaContato.cs
@@ -0,0 +1,60 @@
+using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using Npgsql;
+using System;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace FiapTechChallenge.Contatos.Azure.Consulta
+{
+    public static class ConsultaContato
+    {
+        [FunctionName("ConsultaContato")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {           
+
+            if (!int.TryParse(req.Query["ddd"], out int ddd))
+            {
+                return new BadRequestObjectResult("Parâmetro 'ddd' é obrigatório e deve ser um número válido.");
+            }
+
+            try
+            {
+                using IDbConnection db = new NpgsqlConnection(Environment.GetEnvironmentVariable("POSTGRES_CONNECTION"));
+                string sql = @"
+                            SELECT c.""Id"", 
+                                   c.""Nome"", 
+                                   c.""Telefone"", 
+                                   c.""Email"", 
+                                   c.""RegionalId"" 
+                              FROM ""Contato"" c JOIN ""Regional"" r
+                                ON c.""RegionalId"" = r.""Id""
+                             WHERE r.""Ddd"" = @DDD";
+
+                var contatos = await db.QueryAsync<Contato>(sql, new { DDD = ddd });
+
+                return new OkObjectResult(contatos);
+            }
+            catch (Exception ex)
+            {
+                log.LogError($"Erro ao acessar o banco: {ex.Message}");
+                return new StatusCodeResult(500);
+            }
+        }
+
+        public class Contato
+        {
+            public Guid Id { get; set; }
+            public string Nome { get; set; }
+            public string Telefone { get; set; }
+            public string Email { get; set; }
+            public Guid RegionalId { get; set; }
+        }
+    }
+}
